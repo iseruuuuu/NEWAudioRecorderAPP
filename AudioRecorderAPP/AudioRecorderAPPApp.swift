d4360b0ca03e4897875043104f16d1aa0e8a665f
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2020-11-14 18:37:14 +0900

    Initial Commit

---
+++ AudioRecorderAPP/AudioRecorderAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioRecorderAPPApp.swift
+//  AudioRecorderAPP
+//
+//  Created by user on 2020/11/14.
+//
+
+import SwiftUI
+
+@main
+struct AudioRecorderAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
